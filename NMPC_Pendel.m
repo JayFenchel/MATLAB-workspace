a8fd513882120c286cd9ae89bf40ebef52b086d5
JayFenchel <hannes.heinemann@web.de>
2014-09-25 16:05:51 +0200

    kleine Änderungen am Plot

--- NMPC_Pendel.m
+++ NMPC_Pendel.m
@@ -5,8 +5,8 @@ p.l = 0.5;                          % Laenge des Pendels in [m]
 p.dt = 0.05;                          % Intervall der Messzeiten [s]
 
 tspan = [0:p.dt:10];
-delta = 0.02; % Verzögerung durch Berechnung
-N=10;   % Anzahl prädizierter Schritte
+delta = 0.02; % Verzï¿½gerung durch Berechnung
+N=10;   % Anzahl prï¿½dizierter Schritte
 
 
 %% Anfangsbedingungen
@@ -14,11 +14,11 @@ Phi0=10*(2*pi/360);                % Anfangsauslenkung des Pendels
 %5*(2*pi/360);                    % Beispiel fuer 5 Grad Auslenkung
 x0=[0 0 Phi0 0];                  % Anfangsschaetzung Phi=0 , Phi_Punkt=0
 
-%% Sollgrößen
+%% Sollgrï¿½ï¿½en
 x3_soll=0;
 
 %% Eingangsvektor
-u0=zeros(1,N+1)   % Startwert für die erste Schätzung
+u0=zeros(1,N+1)   % Startwert fï¿½r die erste Schï¿½tzung
 %%
 [t_mess, x_mess] = ode45(@nmpcPendelDGS, tspan(1:40+1), x0, odeset, zeros(1,40+1), p);
 
@@ -29,24 +29,28 @@ subplot(2,1,2)
 plot(t_mess,x_mess(:,3))
 grid on
 
-% [u_opt, fehlersum_opt] = fminunc(@(u) kosten(u, tspan, x0, x3_soll, p), u0);
-% 
-% %% Optimum plotten
-% [t_opt, x_opt] = ode45(@nmpcPendelDGS, tspan, x0, odeset, u_opt, p);
-% 
-% figure(2)
-% subplot(2,1,1)
-% plot(t_opt,u_opt);
-% subplot(2,1,2)
-% plot(t_opt,x_opt(:,3))
-% grid on
+options = optimoptions('fminunc','Algorithm','quasi-newton');
+[u_opt, fehlersum_opt] = fminunc(@(u) kosten(u, tspan(1:1+N), x0, x3_soll, p), u0);
+
+%% Optimum plotten
+[t_opt, x_opt] = ode45(@nmpcPendelDGS, tspan(1:1+N), x0, odeset, u_opt, p);
+
+figure(2)
+subplot(2,1,1)
+plot(sort([t_opt;t_opt+p.dt]),reshape([u_opt;u_opt],1,length(u_opt)*2));
+subplot(2,1,2)
+plot(t_opt,x_opt(:,3))
+grid on
 
 x_mess = x0;
 u_done=0;
 for i=1:1:40
 i
     %% Optimierung
-    [u_opt, fehlersum_opt] = fminunc(@(u) kosten(u, tspan(1:1+N), x_mess(end,:), x3_soll, p), u0);
+    
+    options = optimoptions('fminunc','Algorithm','quasi-newton');
+%     options = optimoptions('fminunc','Algorithm','trust-region');
+    [u_opt, fehlersum_opt] = fminunc(@(u) kosten(u, tspan(1:1+N), x_mess(end,:), x3_soll, p), u0, options);
 
     u_done = [u_done u_opt(2)]
     %% Messung
@@ -56,7 +60,7 @@ end
 %% Optimum plotten
 figure(2)
 subplot(2,1,1)
-plot(t_mess,u_done);
+plot(sort([t_mess;t_mess-p.dt+delta]),reshape([u_done,u_done],1,length(u_done)*2));
 subplot(2,1,2)
 plot(t_mess,x_mess(:,3))
 grid on
@@ -66,7 +70,7 @@ end
 function J = kosten(u, tspan, x0, x3_soll, p)
 
 [t_pred, x_pred] = ode45(@nmpcPendelDGS, tspan, x0, odeset, u, p);
-J = sum((x_pred(:,3)-x3_soll).^2);
+J = sum((x_pred(:,3)-x3_soll).^2)
 end
 
 
